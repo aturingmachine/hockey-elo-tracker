eecd551f71ebfafcea0faaf323a19eb32168fa8a
Donald Johnson <Donald.Johnson@gfs.com>
2019-01-09 08:31:44 -0600

    Issue-8: Enhancement - Create Elo calculator class
    Updates tests to be more robust and clean
    
    Signed-off-by: Donald Johnson <djohnson@solstice.com>

--- server/src/test/kotlin/com/hockey/elo/elotracker/statistics/EloRatingsTest.kt
+++ server/src/test/kotlin/com/hockey/elo/elotracker/statistics/EloRatingsTest.kt
@@ -7,22 +7,36 @@ class EloRatingsTest {
 
     private val player1Elo = 2400
     private val player2Elo = 2000
+    private val player1Ratings = EloRatings(player1Elo)
+    private val player2Ratings = EloRatings(player2Elo)
 
     @Test
-    fun calcEloRating_Player1MatchesPlayer2() {
-        val player1Ratings = EloRatings(player1Elo)
-
+    fun calcWinAgainst_Player1BeatsPlayer2() {
         assertThat(player1Ratings.calcWinAgainst(player2Elo)).isEqualTo(2403)
+    }
+
+    @Test
+    fun calcLossAgainst_Player1LosesToPlayer2() {
         assertThat(player1Ratings.calcLossAgainst(player2Elo)).isEqualTo(2371)
+    }
+
+    @Test
+    fun calcTieAgainst_Player1TiesPlayer2() {
         assertThat(player1Ratings.calcTieAgainst(player2Elo)).isEqualTo(2387)
     }
 
     @Test
-    fun calcEloRating_Player2MatchesPlayer1() {
-        val player2Ratings = EloRatings(player2Elo)
-
+    fun calcWinAgainst_Player2BeatsPlayer1() {
         assertThat(player2Ratings.calcWinAgainst(player1Elo)).isEqualTo(2029)
+    }
+
+    @Test
+    fun calcLossAgainst_Player2LosesToPlayer1() {
         assertThat(player2Ratings.calcLossAgainst(player1Elo)).isEqualTo(1997)
+    }
+
+    @Test
+    fun calcTieAgainst_Player2TiesPlayer1() {
         assertThat(player2Ratings.calcTieAgainst(player1Elo)).isEqualTo(2013)
     }
 }
