05453c1589ae3bfdf69a7156dbe86ce10eabdbd1
Vincent Blom <blomvin@gmail.com>
2019-01-09 06:43:39 -0600

    Set Up DB Connection stuff
    - Also rename match to matchrecord since match is a mysql reserved word
    - set up application.properties to use env vars for db connection

--- server/src/main/kotlin/com/hockey/elo/elotracker/match/model/MatchRecord.kt
+++ server/src/main/kotlin/com/hockey/elo/elotracker/match/model/MatchRecord.kt
@@ -6,14 +6,14 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class Match(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+data class MatchRecord(
+    @Id @GeneratedValue
     var id: Long,
     var playerOneId: Long,
     var playerTwoId: Long,
     var playerOneScore: Int = 0,
     var playerTwoScore: Int = 0,
-    var winner: Long //player Id
+    var winner: Long
 ) {
     constructor() : this (0L, 0L, 0L, 0, 0, 0L)
 }
