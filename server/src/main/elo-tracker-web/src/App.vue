af216049bfffe5f313217dc422392aa6092a57fd
Vincent Blom <blomvin@gmail.com>
2019-01-11 23:01:35 -0600

    Add basic layout and dead links.

--- server/src/main/elo-tracker-web/src/App.vue
+++ server/src/main/elo-tracker-web/src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <v-app>
+
+    <v-navigation-drawer
+      permanent
+      app
+      clipped
+    >
+    <v-list>
+      <v-list-tile
+        v-for="link in links"
+        :key="link.title"
+        :href="link.href"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ link.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ELO</span>
+        <span class="font-weight-light">TRACKER</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
@@ -25,13 +39,29 @@
 
 
 export default {
-  name: 'Elo Tracker',
+  name: 'elo-tracker',
   components: {
     //
   },
   data () {
     return {
-      //
+      links: [
+        {
+          title: 'Users',
+          icon: 'account_circle',
+          href: '/users'
+        },
+        {
+          title: 'Match History',
+          icon: 'assignment',
+          href: '/matches'
+        },
+        {
+          title: 'Elo Chart',
+          icon: 'timeline',
+          href: '/elo'
+        }
+      ]
     }
   }
 }
