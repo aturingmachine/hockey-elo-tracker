fe01e080222e64bfdddc5bdb8e8f902c030f1b65
Donald Johnson <Donald.Johnson@gfs.com>
2019-01-27 22:26:38 -0600

    ELO-40: Enhancement - Update Match History to Support Mutiple Games
    When adding user history, this adds the ability to create a 1200 init record
    for the user to get that first history record in the db
    Adds elo history to be created when a match is complete

--- server/src/main/kotlin/com/hockey/elo/elotracker/elohistory/controller/EloHistoryController.kt
+++ server/src/main/kotlin/com/hockey/elo/elotracker/elohistory/controller/EloHistoryController.kt
@@ -3,16 +3,16 @@ package com.hockey.elo.elotracker.elohistory.controller
 import com.hockey.elo.elotracker.elohistory.model.EloHistoryDTO
 import com.hockey.elo.elotracker.elohistory.repository.models.EloHistoryRecord
 import com.hockey.elo.elotracker.elohistory.service.EloHistoryService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import com.hockey.elo.elotracker.shared.models.GameType
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class EloHistoryController(private val eloHistoryService: EloHistoryService) {
 
-  // TODO: this is untested
-  @GetMapping("/api/v1/elo-history/{userId}")
-  fun retrieveEloHistoryForUser(@PathVariable("userId") userId: Long) : List<EloHistoryDTO> =
-      eloHistoryService.retrieveEloHistoryFor(userId = userId)
+  @GetMapping("/api/v1/users/{userId}/elo-history")
+  fun retrieveEloHistoryForUser(
+          @PathVariable("userId") userId: Long,
+          @RequestParam("gameType") gameType: GameType): List<EloHistoryDTO> =
+          eloHistoryService.retrieveEloHistoryFor(userId = userId, gameType = gameType)
 
 }
--- server/src/main/kotlin/com/hockey/elo/elotracker/elohistory/repository/EloHistoryRepository.kt
+++ server/src/main/kotlin/com/hockey/elo/elotracker/elohistory/repository/EloHistoryRepository.kt
@@ -1,12 +1,13 @@
 package com.hockey.elo.elotracker.elohistory.repository
 
 import com.hockey.elo.elotracker.elohistory.repository.models.EloHistoryRecord
+import com.hockey.elo.elotracker.shared.models.GameType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface EloHistoryRepository: JpaRepository<EloHistoryRecord, Long> {
 
-  fun findAllByUserId(userId: Long): List<EloHistoryRecord>
+  fun findAllByUserIdAndGameType(userId: Long, gameType: GameType): List<EloHistoryRecord>
 
 }
